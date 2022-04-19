8d8d4b3c726f3283b88d97728d7a143aaed7ccb4
Daniil <daniil.vodenejev@khk.ee>
2022-04-19 21:33:25 +0300

    Added API data to project template file

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
+const fetch = require('node-fetch');
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function (req,res){
-    res.render('index')
+const key = 'e187a92b0f0e7b0d085b55e9fbd90abd';
+let city = 'Tartu';
+
+app.get('/', function (req,res) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            //console.log(data);
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            console.log(description);
+            console.log(city);
+            console.log(temp);
+            console.log("QWERT");
+
+    res.render('index', {
+        description: description,
+        city: city,
+        temp: temp
+        })
+    })
+})
+  app.listen(3000)
+/*
+})
+})
+})
+app.post('/', function (res, req){
+let city = req.body.cityname
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+.then((response) => {
+  return response.json()
+})
+.then((date) => {
+  let description = data.weather[0].description
+  let city = data.name
+  let temp = Math.round(parseFloat(data.main.temp)-273.15)
+  console.log(description);
+  console.log(city);
+  console.log(temp);
+  console.log("pppppppppppppppp");
+
+  res.render('index', {
+      description: description,
+      city: city,
+      temp: temp
+  })
+})
 })
 app.listen(3000);
+*/
